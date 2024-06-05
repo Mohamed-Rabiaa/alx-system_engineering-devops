f6c5acb762e8f94a32a2be41736404f6a9a9bbf6
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-06-05 21:06:27 +0300

    Fixing 0-strace_is_your_friend.pp file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
+# This puppet fixes the Apache server
 exec {'fix-wordpress':
-     command => 'sed -i 's/phpp/php/g' /var/www/html/wp-settings.php',
-     path    => '/bin/';
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => ['/bin', '/usr/bin'],
 }
