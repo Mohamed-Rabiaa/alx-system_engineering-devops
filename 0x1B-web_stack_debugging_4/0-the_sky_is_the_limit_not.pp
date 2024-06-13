aa66f9dd8655ae764df5050026f9d1f34fb121ec
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-06-12 18:14:28 -0700

    Fixing code

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,13 @@
 # Increasing the limit of concurrent requests to our server
 exec { 'ulimit-increase' :
-     command     => 'sed -i "s/15/4096/" /etc/default/nginx',
-     path    	 => ['/usr/bin', '/usr/sbin',],
-     }
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/bin', '/usr/local/bin','/bin/',],
+  onlyif  => 'grep -q "15" /etc/default/nginx',
+}
 
-exec { 'restart-nginx' "
-     command     => 'sudo service nginx restart',
-     path        => ['/usr/bin', '/usr/sbin',],
+exec { 'restart-nginx' :
+  command     => 'service nginx restart',
+  path        => ['/usr/bin', '/usr/local/bin','/bin/', '/usr/sbin/'],
+  subscribe   => Exec['ulimit-increase'],
+  refreshonly => true,
 }
